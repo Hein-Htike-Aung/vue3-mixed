36cbda6ebf1048d47bb4dd59856b6f75f27ef2b2
Hein Htike Aung <niziu1151998@gmail.com>
2022-10-16 11:07:54 +0630

    commit

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,211 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Recipes</h1>
+    <button @click="togglePopup">Add new Recipe</button>
+
+    <!-- Items -->
+    <div class="recipes">
+      <div
+        class="card"
+        v-for="(recipe, idx) in $store.state.recipes"
+        :key="idx"
+      >
+        <h2>{{ recipe.title }}</h2>
+        <p>{{ recipe.description }}</p>
+        <router-link :to="{ name: 'recipe', params: { slug: recipe.slug } }">
+          <button>View Recipe</button>
+        </router-link>
+      </div>
+    </div>
+
+    <!-- Popup -->
+    <div class="add-recipe-popup" v-if="popupOpen">
+      <div class="popup-content">
+        <h2>Add new Recipe</h2>
+        <form @submit.prevent="addNewRecipe">
+          <div class="group">
+            <label>Title</label>
+            <input type="text" required v-model="newRecipe.title" />
+          </div>
+
+          <div class="group">
+            <label>Description</label>
+            <textarea v-model="newRecipe.description" required></textarea>
+          </div>
+
+          <div class="group">
+            <label>Ingredients</label>
+            <div
+              class="ingredient"
+              v-for="i in newRecipe.ingredientRows"
+              :key="i"
+            >
+              <input type="text" v-model="newRecipe.ingredients[i - 1]" />
+            </div>
+            <button type="button" @click="addNewIngredient">
+              Add Ingredient
+            </button>
+          </div>
+
+          <div class="group">
+            <label>Method</label>
+            <div class="ingredient" v-for="i in newRecipe.methodRows" :key="i">
+              <textarea v-model="newRecipe.method[i - 1]"></textarea>
+            </div>
+            <button type="button" @click="addNewStep">Add Step</button>
+          </div>
+
+          <button type="submit">Add Recipe</button>
+          <button @click="togglePopup" type="button">Close</button>
+        </form>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import { ref } from "vue";
+import { useStore } from "vuex";
 
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
+  name: "HomeView",
+  components: {},
+  setup() {
+    const newRecipe = ref({
+      title: "",
+      description: "",
+      ingredients: [],
+      method: [],
+      ingredientRows: 1,
+      methodRows: 1,
+    });
+    const popupOpen = ref(false);
+    const store = useStore();
+
+    const togglePopup = () => {
+      popupOpen.value = !popupOpen.value;
+    };
+
+    const addNewIngredient = () => {
+      newRecipe.value.ingredientRows++;
+    };
+
+    const addNewStep = () => {
+      newRecipe.value.methodRows++;
+    };
+
+    const addNewRecipe = () => {
+      newRecipe.value.slug = newRecipe.value.title
+        .toLowerCase()
+        .replace(/\s/g, "-");
+
+      if (newRecipe.value.slug === "") {
+        alert("Please enter a title");
+        return;
+      }
+
+      store.commit("ADD_RECIPE", { ...newRecipe.value });
+
+      newRecipe.value = {
+        title: "",
+        description: "",
+        ingredients: [],
+        method: [],
+        ingredientRows: 1,
+        methodRows: 1,
+      };
+
+      togglePopup();
+    };
+
+    return {
+      newRecipe,
+      popupOpen,
+      togglePopup,
+      addNewIngredient,
+      addNewStep,
+      addNewRecipe,
+    };
+  },
+};
 </script>
+
+<style scoped>
+.home {
+  padding: 1rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+h1 {
+  font-size: 3rem;
+  margin-bottom: 32px;
+}
+.recipes {
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+}
+.recipes .card {
+  padding: 1rem;
+  border-radius: 5px;
+  margin: 1rem;
+  background-color: #081c33;
+}
+.recipes .card h2 {
+  font-size: 2rem;
+  margin-bottom: 1rem;
+}
+.recipes .card p {
+  font-size: 1.125rem;
+  line-height: 1.4;
+  margin-bottom: 1rem;
+}
+.add-recipe-popup {
+  position: fixed;
+  top: 10px;
+  left: 25%;
+  background-color: transparent;
+  width: 50%;
+  margin: auto;
+  height: 100%;
+  overflow: scroll;
+  scrollbar-width: none;
+}
+.add-recipe-popup .popup-content {
+  background-color: #081c33;
+  padding: 2rem;
+  border-radius: 1rem;
+  width: 100%;
+  min-height: fit-content;
+  max-width: 768px;
+  box-shadow: 10px 4px 6px -1px rgba(0, 0, 0, 0.2),
+    0 2px 4px -1px rgba(0, 0, 0, 0.06);
+}
+.popup-content h2 {
+  font-size: 2rem;
+  margin-bottom: 1rem;
+}
+.popup-content .group {
+  margin-bottom: 1rem;
+}
+.popup-content .group label {
+  display: block;
+  margin-bottom: 0.5rem;
+}
+.popup-content .group input,
+.popup-content .group textarea {
+  display: block;
+  width: 100%;
+  padding: 0.5rem;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  margin-bottom: 1rem;
+}
+.popup-content .group textarea {
+  height: 100px;
+  resize: none;
+}
+.popup-content button[type="submit"] {
+  margin-right: 1rem;
+}
+</style>
---
+++ src/views/RecipeView.vue
@@ -0,0 +1,79 @@
+<template>
+  <div class="recipe">
+    <router-link :to="{ name: 'home' }">&lt; Back</router-link>
+    <h1>{{ recipe.slug }}</h1>
+    <p class="desc">{{ recipe.description }}</p>
+    <hr />
+    <div class="ingredients">
+      <h3>Ingredients</h3>
+      <ul>
+        <li v-for="(ingredient, idx) in recipe.ingredients" :key="idx">
+          {{ ingredient }}
+        </li>
+      </ul>
+    </div>
+
+    <div class="method">
+      <h3>Method</h3>
+      <ol>
+        <li v-for="(step, idx) in recipe.method" :key="idx">
+          <span v-html="cleanText(step)"></span>
+        </li>
+      </ol>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "RecipeView",
+  computed: {
+    recipe() {
+      return this.$store.state.recipes.find(
+        (r) => r.slug === this.$route.params.slug
+      );
+    },
+  },
+  methods: {
+    cleanText(text) {
+      return text.replace(/\n/g, "<br />");
+    },
+  },
+};
+</script>
+
+<style scoped>
+.recipe {
+  padding: 1rem;
+  max-width: 768px;
+  margin: 0 auto;
+}
+.desc {
+  font-size: 1.125rem;
+  line-height: 1.4;
+  margin-bottom: 1rem;
+}
+hr {
+  margin-bottom: 1rem;
+}
+h3 {
+  margin-bottom: 1rem;
+}
+.ingredients {
+  padding: 1rem;
+  background-color: #081c33;
+  border-radius: 0.5rem;
+  margin-bottom: 2rem;
+}
+.ingredients ul li {
+  list-style-position: inside;
+  line-height: 1.4;
+  margin-bottom: 1rem;
+}
+.method ol li {
+  margin-bottom: 2rem;
+  padding-bottom: 1rem;
+  list-style-position: inside;
+  border-bottom: 1px solid #eee;
+}
+</style>
